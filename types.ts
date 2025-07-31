1112e9aff99acf4de3d29a8bb69d41e6487518c3
Nicholas D. Smith <dev.nick15@yahoo.com>
2025-07-30 20:52:26 -0500

    Made some more type changes. More to come

--- types.ts
+++ types.ts
@@ -1,48 +1,50 @@
-import { NextFunction, Request, Response } from "express";
-import { ArcjetDecision } from "@arcjet/node";
+import {NextFunction, Request, Response} from "express";
+import {ArcjetDecision} from "@arcjet/node";
 import z from "zod";
 
 export type DataPayloadLocation = "body" | "query" | "params";
 
 export type ArcjetDecisionProps<Req> = (args: {
-	req: Req;
-	fingerprint: string;
+    req: Req;
+    fingerprint: string;
 }) => Promise<ArcjetDecision>;
 
 export type ExpressRequest<
-	ZSchema extends z.ZodType,
-	PayloadLocation extends DataPayloadLocation | undefined
+    ZSchema extends z.ZodType,
+    PayloadLocation extends DataPayloadLocation | undefined
 > = PayloadLocation extends "body" // When the location is req.body
-	? Request<any, any, z.infer<ZSchema>>
-	: // When the location is req.params
-	PayloadLocation extends "params"
-	? Request<z.infer<ZSchema>, any, any>
-	: // When the location is req.query
-	PayloadLocation extends "query"
-	? Request<any, any, any, z.infer<ZSchema>>
-	: Request;
+    ? Request<any, any, ProperTypePicker<ZSchema, {}>>
+    : // When the location is req.params
+    PayloadLocation extends "params"
+        ? Request<ProperTypePicker<ZSchema, {}>, any, any>
+        : // When the location is req.query
+        PayloadLocation extends "query"
+            ? Request<any, any, any, ProperTypePicker<ZSchema, {}>>
+            :  Request;
+
+type ProperTypePicker<ZSchema extends z.ZodType, ReturnType = {}> = z.infer<ZSchema> extends unknown ? ReturnType : z.infer<ZSchema>
 
 export type SafeApiHandlerProps<
-	ZSchema extends z.ZodType,
-	PayloadLocation extends DataPayloadLocation
+    ZSchema extends z.ZodType,
+    PayloadLocation extends DataPayloadLocation
 > = {
-	handler: (props: {
-		req: ExpressRequest<ZSchema, PayloadLocation>;
-		res: Response;
-		next: NextFunction;
-		decision?: ArcjetDecision;
-	}) => void;
-	arcjetDecision?: (
-		req: ExpressRequest<ZSchema, PayloadLocation>,
-		fingerprint: string
-	) => Promise<ArcjetDecision>;
-	validator?: ZSchema;
-	location?: PayloadLocation;
+    handler: (props: {
+        req: ExpressRequest<ZSchema, PayloadLocation>;
+        res: Response;
+        next: NextFunction;
+        decision?: ArcjetDecision;
+    }) => void;
+    arcjetDecision?: (
+        req: ExpressRequest<ZSchema, PayloadLocation>,
+        fingerprint: string
+    ) => Promise<ArcjetDecision>;
+    validator?: ZSchema;
+    location?: PayloadLocation;
 };
 
 export type StructuredResponseType<Payload extends object> = {
-	statusCode?: number;
-	date: string;
-	environment: "development" | "production" | "preview";
-	data: Payload;
+    statusCode?: number;
+    date: string;
+    environment: "development" | "production" | "preview";
+    data: Payload;
 };
